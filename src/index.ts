9b2b6b58c8240a5e410ca52c877805e61b1752ac
VVMD-team <vvmd.team@gmail.com>
2025-07-31 14:58:42 +0300

    Add origins

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,21 @@ import webflowRoutes from "./routes/boats.routes";
 
 const app = express();
 
+const allowedOrigins = [
+  "https://verge3d-boat-ui.webflow.io",
+  "https://grand-3d-test.vercel.app",
+  "https://constructor.grandboats.com",
+];
+
 app.use(
   cors({
-    origin: "https://verge3d-boat-ui.webflow.io",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
