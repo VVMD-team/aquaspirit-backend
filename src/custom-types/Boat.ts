2b2c20b9c9426727c204fccb189e71764767ab6b
VVMD-team <vvmd.team@gmail.com>
2025-08-13 13:57:20 +0300

    f

--- src/custom-types/Boat.ts
+++ src/custom-types/Boat.ts
@@ -4,22 +4,15 @@ export type BoatDynamicFields = {
   [key in `tab-${number}-color-${number}` | `option-${number}`]?: string;
 };
 
-export type BoatDynamicFieldsForGET = {
-  [key in
-    | `tab-${number}-color-${number}`
-    | `option-${number}-text`
-    | `option-${number}-code`]?: string;
-};
-
 export type BoatBase = {
   name: string;
   country: COUNTRIES;
   email: string;
   phone: string;
   city: string;
-  comment: string; // keep key as "comment" (backend expects this exact name)
+  comment: string;
   screen: string;
   link: string;
 };
 
-export type Boat = BoatBase & BoatDynamicFields & BoatDynamicFieldsForGET;
+export type Boat = BoatBase & BoatDynamicFields;
